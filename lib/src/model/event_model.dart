ec6adf9457c5848ac470d536b361f5acfa3e4dd0
fujimotokk <kokoro625@gmail.com>
2022-12-12 12:22:58 +0900

    質問募集

--- lib/src/model/event_model.dart
+++ lib/src/model/event_model.dart
@@ -8,7 +8,7 @@ class Event {
   final String gender;
   final int age;
   final int people;
-  //final String genre;
+  final String genre;
   final String background; // 募集した理由
   /*final String? startplace;
   final String? starttime;
@@ -17,6 +17,9 @@ class Event {
   final String favorite;
   final String fanhistory;
   final String participation;
+  final String question1;
+  final String question2;
+  final String question3;
 
   Event._(
       {required this.name,
@@ -26,7 +29,7 @@ class Event {
       required this.gender,
       required this.age,
       required this.people,
-      //required this.genre,
+      required this.genre,
       required this.background,
       /*required this.startplace,
       required this.starttime,
@@ -34,27 +37,32 @@ class Event {
       required this.goaltime}),*/
       required this.favorite,
       required this.fanhistory,
-      required this.participation});
+      required this.participation,
+      required this.question1,
+      required this.question2,
+      required this.question3});
 
   factory Event.fromSnapshot(DocumentSnapshot snapshot) {
     final data = snapshot.data() as Map<String, dynamic>;
     return Event._(
-      name: data["name"],
-      date: data["date"].toDate(), // timestamp型からdatetime型へ
-      prefec: data["prefec"],
-      place: data["place"],
-      gender: data["gender"],
-      age: data["age"],
-      people: data["num"],
-      //genre: data["genre"],
-      background: data["background"],
-      /*startplace: data["startPlace"],
+        name: data["name"],
+        date: data["date"].toDate(), // timestamp型からdatetime型へ
+        prefec: data["prefec"],
+        place: data["place"],
+        gender: data["gender"],
+        age: data["age"],
+        people: data["num"],
+        genre: data["genre"],
+        background: data["background"],
+        /*startplace: data["startPlace"],
       starttime: data["startTime"],
       goalplace: data["goalPlace"],
       goaltime: data["goalTime"],*/
-      favorite: data["favorite"],
-      fanhistory: data["fanhistory"],
-      participation: data["participation"],
-    );
+        favorite: data["favorite"],
+        fanhistory: data["fanhistory"],
+        participation: data["participation"],
+        question1: data["question1"],
+        question2: data["question2"],
+        question3: data["question3"]);
   }
 }
